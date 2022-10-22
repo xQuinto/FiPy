5d253407a6864323e4f6ac154dd17f340368a8fb
xQuinto <quintine_eier@hotmail.com>
2022-10-22 20:37:48 +0200

    laat alle uitgaven zien per categorie in een bar graph en deze is op te slaan als afbeelding

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pandas as pd
 #from pandas_datareader import data
-from kivy.app import App
+#from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
-
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from backend_kivyagg import FigureCanvasKivyAgg
+import matplotlib.pyplot as plt
 
 def bereken_uitgave_per_kostenpost(kostenpost):
     mask = bankafschrift_af["Naam / Omschrijving"].str.contains(kostenpost)
@@ -92,22 +95,54 @@ kosten_ing_totaal = (bereken_totaal(kosten_ing_list))
 
 uitgaven_totaal = boodschappen_totaal + shoppen_totaal + kleding_totaal + verzorging_totaal + geld_opgenomen_totaal + uit_eten_totaal + baby_totaal + betaalverzoeken_totaal + zwangerschap_totaal + eten_onderweg_totaal + dagje_uit_totaal + af_naar_eigen_rekening_totaal + kosten_ing_totaal
 uitgaven_overig = bankafschrift_af_zonder_naar_spaar["Bedrag (EUR)"].sum() - uitgaven_totaal
+#  print(uitgaven_overig)
 
-#print(uitgaven_overig)
+# MATPLOTLIB TEST
+# Define what we want to graph
+x = ["Boodschappen", "shoppen", "kleding", "verzorging", "geldautomaat", "uit eten", "bebe", "betaalverzoeken", "zwanger", "onderweg eten", "dagje uit", "eigen rekening", "ing kosten", "eten bestellen"]
+y = [boodschappen_totaal, shoppen_totaal, kleding_totaal, verzorging_totaal, geld_opgenomen_totaal, uit_eten_totaal, baby_totaal, betaalverzoeken_totaal, zwangerschap_totaal, eten_onderweg_totaal, dagje_uit_totaal, af_naar_eigen_rekening_totaal, kosten_ing_totaal, eten_bestellen_totaal]
+
+plt.plot(x, y)
+#plt.bar(x, y)
+#plt.pie(y, labels=x)
+plt.ylabel("Bedrag in euro's")
+plt.xlabel("Kostenpost")
+plt.title("Uitgaven per kostenpost")
+
+
+class Matty(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        box = self.ids.box
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
+
+    def save_it(self):
+        name = self.ids.namer.text
+        if name:
+            plt.savefig(name)
+
+
+#  WORDT NU NIET GEBRUIKT
 class MyGrid(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
-    #print(uitgaven_totaal)
+    inkomsten = ObjectProperty(None)
+    uitgaven = ObjectProperty(None)
+    #  print(uitgaven_totaal)
 
     def btn(self):
-        print("Name:", self.name.text, "email:", self.email.text)
-        self.name.text = str(uitgaven_totaal)
-        self.email.text = str(bankafschrift_bij["Bedrag (EUR)"].sum())
+        #  print("Name:", self.name.text, "email:", self.email.text)
+        self.inkomsten.text = str(bankafschrift_bij["Bedrag (EUR)"].sum())
+        self.uitgaven.text = str(uitgaven_totaal)
 
 
-class ExcelFinancienApp(App):
+class ExcelFinancienApp(MDApp):
     def build(self):
-        return MyGrid()
+        #  return MyGrid()
+
+        #  self.theme_cls.theme_style = "Dark"
+        #  self.theme_cls.primary_palette = "BlueGray"
+        Builder.load_file('matplotlibtest.kv')
+        return Matty()
 
 
 if __name__ == "__main__":
